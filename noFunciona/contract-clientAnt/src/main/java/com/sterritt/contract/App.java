07e1df07a97ef0a6878492ece8c9c3f6f1717975
osukaru16 <osukaru16@gmail.com>
2018-01-22 09:20:12 +0100

    Add files via upload

---
+++ noFunciona/contract-clientAnt/src/main/java/com/sterritt/contract/App.java
@@ -0,0 +1,8 @@
+package com.sterritt.contract;
+
+
+
+
+public class App {
+
+}
---
+++ noFunciona/contract-cliente/src/main/java/com/starritt/contract/client/ContractClient.java
@@ -0,0 +1,28 @@
+package com.starritt.contract.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestOperations;
+
+import com.starritt.contract.dto.Contract;
+
+@Component
+public class ContractClient {
+	
+	@Autowired
+	private RestOperations restOperations;
+	
+	private final String url;
+	
+	@Autowired
+	public ContractClient(@Value("${contract.service.url}") final String url) {
+		this.url = url;
+	}
+	
+	
+	public Contract getContract(final int contractNumbre) {
+		return restOperations.getForObject(url, Contract.class, contractNumbre);
+	}
+
+}
